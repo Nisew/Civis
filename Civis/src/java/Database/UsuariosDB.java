1e5c8fdf813bc5b7bdc7a1642e0778aced0754c6
Alejandro <alejandro.guti.ferreria@gmail.com>
2019-11-25 11:11:36 +0100

    DB in progress;

--- Civis/src/java/Database/UsuariosDB.java
+++ Civis/src/java/Database/UsuariosDB.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Database;
 
-/**
- *
- * @author Usuario
- */
+import Entities.Usuarios;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsuariosDB {
+ 
+    Connection con;
     
+    public boolean addUsuario(Usuarios u) throws SQLException {
+    
+        boolean existe = false;
+        
+        String sqlInsert = "INSERT INTO usuarios (usuario, contrasenya, nombre, apellidos, fecha_nacimiento, telefono, correo) VALUES (?,?,?,?,?,?,?)";
+        PreparedStatement ps1 = con.prepareStatement(sqlInsert);
+        
+        String sqlSelect = "SELECT usuario FROM usuarios";
+        PreparedStatement ps2 = con.prepareStatement(sqlSelect);
+        
+        ResultSet rs = ps2.executeQuery();
+        
+        while (rs.next()) {            
+            if (u.getUsuario().equals(rs.getString("usuario"))) {
+                existe = true;
+            }
+        }
+        
+        if (!existe) {
+            ps1.setString(1, u.getUsuario());
+            ps1.setString(2, u.getPasswd());
+            ps1.setString(3, u.getNombre());
+            ps1.setString(4, u.getApellidos());
+            ps1.setDate(5, u.getFechaNacimiento());
+            ps1.setInt(6, u.getTelefono());
+            ps1.setString(7, u.getCorreo());
+        
+            ps1.executeUpdate();
+        }
+        return existe;
+    }
+    
+    public void startSession() {}
 }
---
+++ Civis/src/java/Entities/Usuarios.java
@@ -0,0 +1,86 @@
+package Entities;
+
+import java.sql.Date;
+
+public class Usuarios {
+ 
+    String usuario;
+    String passwd;
+    String nombre;
+    String apellidos;
+    Date fechaNacimiento;
+    int telefono;
+    String correo;
+
+    public Usuarios(String usuario, String passwd, String nombre, String apellidos, Date fechaNacimiento, int telefono, String correo) {
+        this.usuario = usuario;
+        this.passwd = passwd;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String corre) {
+        this.correo = corre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" + "usuario=" + usuario + ", passwd=" + passwd + ", nombre=" + nombre + ", apellidos=" + apellidos + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", correo=" + correo + '}';
+    }
+    
+}
